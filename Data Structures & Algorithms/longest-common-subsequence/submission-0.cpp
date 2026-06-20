076f37a2e4edd73c6db7fb0ea319c24d03310b83
Shivanadri Abhinav <abhinavs1256@gmail.com>
2026-06-20 14:37:37 +0530

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string s, string t) {
+        int n=s.size(),m=t.size();
+        vector<int> prev(m+1,0),cur(m+1,0);
+       // prev[0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    cur[j]=1+prev[j-1];
+                }
+                else{
+                    cur[j]=max(cur[j-1],prev[j]);
+                }
+            }
+            prev=cur;
+        }
+        return prev[m];
+    }
+};
